761f8ae45824e4af105e9edead74c812ec750685
Emtiaz2000 <62500633+Emtiaz2000@users.noreply.github.com>
2021-04-04 19:42:30 +0600

    Delete routes directory

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {getRegisterControler} = require('../controlers/authControler')
-
-router.get('/register',getRegisterControler)
-
-
-module.exports = router
